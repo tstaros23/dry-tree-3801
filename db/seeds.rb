7cb47b57d27d65ddff23c81a48960d22a83d9d8a
Ted Staros <tstaros23@gmail.com>
2021-11-12 15:37:12 -0700

    Finish test refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
-dr_bailey = hospital.doctors.create(name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University")
+dr_bailey = hospital.doctors.create(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+ted = dr_bailey.patients.create(name: 'Ted', age: 29)
+DoctorPatient.create!(patient_id: ted.id, doctor_id: dr_bailey.id)
--- spec/features/hospital/show_spec.rb
+++ spec/features/hospital/show_spec.rb
@@ -6,12 +6,16 @@ RSpec.describe 'Show page' do
     hospital = Hospital.create!(name:'Hells Pass')
     doctor = Doctor.create!(name: 'Ted', specialty: 'Hambones', university: 'Mizzou', hospital_id: hospital.id)
     doctor2 = Doctor.create!(name: 'Bob', specialty: 'Brains', university: 'New Jersey', hospital_id: hospital.id)
-    doctor2 = Doctor.create!(name: 'Chuck', specialty: 'Brains', university: 'New Jersey', hospital_id: hospital.id)
+    doctor3 = Doctor.create!(name: 'Charlie', specialty: 'Arm pits', university: 'New Jersey', hospital_id: hospital.id)
+
+    hospital2 = Hospital.create!(name:'Heavens Pass')
+    doctor2 = Doctor.create!(name: 'Chuck', specialty: 'More Arm Pits', university: 'New Jersey', hospital_id: hospital2.id)
 
     visit "/hospitals/#{hospital.id}"
 
     expect(page).to have_content(hospital.name)
-    expect(page).to have_content('3')
+    expect(page).not_to have_content(hospital2.name)
+    expect(page).to have_content(3)
     expect(page).to have_content('New Jersey', :count => 1)
     expect(page).to have_content('Mizzou')
   end
